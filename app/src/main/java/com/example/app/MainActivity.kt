13fc5ba337ee02704ff39bcf936b9f45ad3f8617
jeanhenriquebarbosadearaujo-lang <jeanhenriquebarbosadearaujo@gmail.com>
2026-07-18 02:52:41 +0000

    App Animador de Traço

---
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/animador-traco-real.html")
+    }
+}
